410ef6f114ba2949008ab1d5bbfa9656cbe6d9e5
joanqa3 <joanqa3@gmail.com>
2022-10-13 11:44:21 +0200

    First commit

---
+++ example/base_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:base_dart/base_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/base_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library base_dart;
+
+export 'src/base_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
